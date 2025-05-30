c1364ce339532554456e3ce177ab4f80ce995f3c
hatif-2003 <hatif940@gmail.com>
2025-05-30 22:42:33 +0530

    taldwin ka kaam complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+     @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1 class="text-red-600">%0000n</h1>
     
 </body>
 </html>
